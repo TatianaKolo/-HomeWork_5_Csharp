8932c7361c17d9a7d54a3bba9791b5f5a58c808b
TatianaKolo <manila-nick@yandex.ru>
2022-11-28 09:25:26 +0300

    ready task36+

--- task36/Program.cs
+++ task36/Program.cs
@@ -1,6 +1,5 @@
 ﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
 стоящих на нечётных позициях.
-
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
@@ -10,7 +9,7 @@ int size = Convert.ToInt32(Console.ReadLine());
 int [] array = new int [size];
 for(int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(-100, 100);}
+    array[i] = new Random().Next(0, 1000);}
 int sum = 0;
 for (int i = 0; i < array.Length; i++)
 {
